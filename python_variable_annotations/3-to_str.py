6d7aabef78299558bd6ef10a40df22bef7ff62ec
ucflumm <ucflumm@gmail.com>
2024-01-14 15:36:34 +1100

    added logic

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """This module contains the function for task 3"""
-from typing import Union, Any, Mapping, Tuple, TypeVar
 
 
-def to_str(n: Union[int, float]) -> str:
+def to_str(n: float) -> str:
     """This function returns the string representation of the argument"""
     return str(n)
