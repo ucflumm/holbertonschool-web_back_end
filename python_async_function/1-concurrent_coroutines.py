71f2a84c555a0827bd6ea9e35ad76002729a80e6
ucflumm <ucflumm@gmail.com>
2024-01-14 17:09:31 +1100

    Add wait_n function to perform concurrent coroutines

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Import wait_random from 0-basic_async_syntax.
+    Write a function (do not create an async function, use the regular function
+    syntax to do this) task_wait_random that takes an integer max_delay and
+    returns a asyncio.Task.
+"""
+import asyncio
+import random
+from typing import List
+task_wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ Import wait_random from 0-basic_async_syntax.
+        Write a function (do not create an async function, use the regular
+        function syntax to do this) task_wait_random that takes an integer
+        max_delay and returns a asyncio.Task.
+    """
+    delays = []
+    for i in range(n):
+        delays.append(await task_wait_random(max_delay))
+    return delays
