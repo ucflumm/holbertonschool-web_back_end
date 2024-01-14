0f6bd3e5369039c311da6dc6136519d04f439943
ucflumm <ucflumm@gmail.com>
2024-01-14 15:56:06 +1100

    Add comments to make_multiplier function

--- python_variable_annotations/8-main.py
+++ python_variable_annotations/8-main.py
@@ -2,5 +2,7 @@
 
 make_multiplier = __import__('8-make_multiplier').make_multiplier
 print(make_multiplier.__annotations__)
+# this makes the multiplier function that multiplies by 2.22
 fun = make_multiplier(2.22)
+# this call of fun multiplies arg 2.22 by the multiplier 2.22
 print("{}".format(fun(2.22)))
