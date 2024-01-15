ab0d3b56d987b42817785d1fae3c4dd168649b21
ucflumm <ucflumm@gmail.com>
2024-01-15 12:39:48 +1100

    added func async comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+""" contains coroutine async_comprehension """
+import asyncio
+import random
+from typing import Generator, List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """ coroutine that collects 10 random numbers using an async comprehensing
+        Returns:
+            10 random numbers
+    """
+    return [i async for i in async_generator()]
