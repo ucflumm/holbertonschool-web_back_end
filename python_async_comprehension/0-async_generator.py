2b0e1c4e990a7999b9a4975cea57bf97edec8262
ucflumm <ucflumm@gmail.com>
2024-01-15 12:36:41 +1100

    added func async_generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+""" Async Generator """
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """ Async Generator """
+    for _ in range(10):
+        # async wait 1 second
+        await asyncio.sleep(1)
+        # yield random number between 0 and 10
+        yield random.uniform(0, 10)
