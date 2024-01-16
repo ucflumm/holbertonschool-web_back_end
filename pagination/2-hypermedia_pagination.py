4705a118fa23ac994888b745e2d6c3553ca0343c
ucflumm <ucflumm@gmail.com>
2024-01-16 13:32:03 +1100

    Add hypermedia pagination functionality

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+""" contains server class """
+import csv
+import math
+from typing import List, Dict, Union, Any
+from functools import reduce
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """ returns a tuple of size two containing a start index
+        and an end index
+        corresponding to the range of indexes to return in a list for those
+        particular pagination parameters
+        The page number is 1-indexed
+        Returns:
+            tuple of start and end indexes
+    """
+    return ((page - 1) * page_size, page * page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ gets page from dataset
+            Args:
+                page: page number
+                page_size: number of items per page
+            Returns:
+                list of items
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        start, end = index_range(page, page_size)
+        return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """
+        returns a dictionary containing the following key-value pairs:
+        page_size: the length of the returned dataset page
+        page: the current page number
+        data: the dataset page (equivalent to return from previous task)
+        next_page: number of the next page, None if no next page
+        prev_page: number of the previous page, None if no previous page
+        total_pages: the total number of pages in the dataset as an integer
+        """
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        return {
+            'page_size': page_size,
+            'page': page,
+            'data': self.get_page(page, page_size),
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': total_pages
+        }
