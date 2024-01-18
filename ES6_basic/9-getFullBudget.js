d06f51409d1c346e2eb84272d306c2b79ec882fb
ucflumm <ucflumm@gmail.com>
2024-01-18 14:09:24 +1100

    Fix import statement in getFullBudgetObject function

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,5 +1,5 @@
-import getBudgetObject from './7-getBudgetObject.js';
 /* eslint-disable */
+import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
