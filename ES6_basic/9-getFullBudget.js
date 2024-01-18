1f198a689d0492c785c63ef8b726a0a2a754001b
ucflumm <ucflumm@gmail.com>
2024-01-18 14:08:03 +1100

    Add eslint-disable comment to getFullBudgetObject function

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,4 +1,5 @@
 import getBudgetObject from './7-getBudgetObject.js';
+/* eslint-disable */
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
