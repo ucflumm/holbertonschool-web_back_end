693741c8e3a01ae4712abda4c13f6e1b7edb3cd0
ucflumm <ucflumm@gmail.com>
2024-01-18 12:26:38 +1100

    Refactor ES6_basic/0-constants.js and ES6_basic/0-main.js

--- ES6_basic/0-main.js
+++ ES6_basic/0-main.js
@@ -1,3 +1,6 @@
 import { taskFirst, taskNext } from './0-constants.js';
 
 console.log(`${taskFirst()} ${taskNext()}`);
+import { taskFirst, taskNext } from './0-constants.js';
+
+console.log(`${taskFirst()} ${taskNext()}`);
