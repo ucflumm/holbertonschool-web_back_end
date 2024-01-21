4f064ae8f37f588d205bd4c84e1744391b62bf4e
ucflumm <ucflumm@gmail.com>
2024-01-21 15:39:17 +1100

    added placeholders

--- ES6_classes/create_js.sh
+++ ES6_classes/create_js.sh
@@ -6,3 +6,16 @@ do
   chmod u+x "${i}-main.js"
 done
 
+touch "0-classroom.js"
+touch "1-make_classrooms.js"
+touch "2-hbtn_course.js"
+touch "3-currency.js"
+touch "4-pricing.js"
+touch "5-building.js"
+touch "6-sky_high.js"
+touch "7-airport.js"
+touch "8-hbtn_field.js"
+touch "9-hoisting.js"
+touch "10-car.js"
+
+ls -la
