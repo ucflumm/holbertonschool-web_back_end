a108a213602e0d49ed7207bab6ba35cbc97d121f
ucflumm <ucflumm@gmail.com>
2024-01-21 21:49:16 +1100

    fixed missing assignment in constructor

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,11 +1,13 @@
 export default class Currency {
   constructor(code, name) {
     if (typeof code != 'string') {
-      throw TypeError('Code must be string');
+      throw TypeError('Code must be a string');
     }
     if (typeof name != 'string') {
       throw TypeError('Name must be a string');
     }
+    this._code = code;
+    this._name = name;
   }
 
   get code() {
