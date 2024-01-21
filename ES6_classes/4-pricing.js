b9dfdf5243ddd75a90892fc94a977a0a0e6db962
ucflumm <ucflumm@gmail.com>
2024-01-21 21:52:41 +1100

    Add Pricing class with amount, currency, and conversion methods

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,50 @@
+import Currency from "./3-currency.js";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== "number") {
+      throw TypeError("Amount must be a number");
+    }
+    if (!(currency instanceof Currency)) {
+      throw TypeError("Currency must be a Currency");
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== "number") {
+      throw TypeError("Amount must be a number");
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw TypeError("Currency must be a Currency");
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== "number") {
+      throw TypeError("Amount must be a number");
+    }
+    if (typeof conversionRate !== "number") {
+      throw TypeError("ConversionRate must be a number");
+    }
+    return amount * conversionRate;
+  }
+}
