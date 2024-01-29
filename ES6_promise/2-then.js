703625eabb0554f0cb5c1f0657590d1f0e30df68
ucflumm <ucflumm@gmail.com>
2024-01-29 12:04:36 +1100

    Refactor handleResponseFromAPI function

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,8 +1,10 @@
+/* eslint-disable */
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({
-    status: 200,
-    body: 'success',
-  }))
-  .catch(() => Error())
-  .finally(() => console.log('Got a response from the API'));
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
