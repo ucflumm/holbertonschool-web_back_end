9e683d49cfa5e29c22ace40b1e96ef5873d33791
ucflumm <ucflumm@gmail.com>
2024-01-29 12:25:07 +1100

    Refactor handleProfileSignup function to handle rejected promises

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from "./4-user-promise.js";
+import uploadPhoto from "./5-photo-reject.js";
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => {
+      for (const value of values) {
+        if (value.status === "rejected") {
+          value.value = `Error: ${value.reason.message}`;
+          delete value.reason;
+        }
+      }
+      return values;
+    });
+}
